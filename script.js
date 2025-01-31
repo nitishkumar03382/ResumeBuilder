d7c6b5c489a641985f7c9acd108471b556031472
manjeet7370 <kumarmanjeet50728@gmail.com>
2025-01-31 19:34:51 +0530

    local storage added

--- script.js
+++ script.js
@@ -1,119 +1,191 @@
-// name_inp = 
-function display(inp, op) {
 
-
-   if(inp == inp_phone){
-        op.innerText = "Phone: +91 " + inp.value; 
-    }
-    else if(inp == inp_cgpa){
-        op.innerText = "CGPA  :-   " + inp.value; 
-    }
-    else
-    op.innerHTML = inp.value
-}
-let inp_name = document.getElementById("input-name")
-document.getElementById('input-name').addEventListener('input', () => {
-    const inputString = document.getElementById('input-name').value.trim(); // User input ko trim karte hain
+new_resume = document.getElementById("NEW-RESUME");
+new_resume.addEventListener('click', ()=>{
+    localStorage.clear();
+    location.reload();
     
-    // if (!inputString) {
-    //     document.getElementById('result').textContent = "Please enter a valid string.";
-    //     return;
-    // }
+   
 
-    // Pehla letter uppercase aur baaki lowercase
+})
+
+
+
+function display(inpElement, opElement, key) {
+    if (!inpElement || !opElement) return; 
+    inp_phone = document.getElementById("input-phone")
+    
+       if(inpElement == inp_phone){
+        
+        opElement.innerText = "Phone: +91 " + inpElement.value;
+        value = "Phone: +91 " +  inpElement.value;
+        localStorage.setItem(key, value);
+        return; 
+    }
+   inp_collage = document.getElementById("input-collage")
+    if( inp_collage == inpElement){
+       opElement.textContent = inp_collage.value;
+        text = inpElement.value
+        localStorage.setItem(key, text)
+        return;
+    }
+
+
+     inp_cgpa = document.getElementById("input-cgpa");
+      if(inpElement === inp_cgpa){
+        opElement.textContent = "CGPA  :-   " + inpElement.value;
+        value =  "CGPA  :-   " + inp.value;
+        localStorage(key, value);
+        return;
+    }
+
+    const inputString = inpElement.value.trim(); 
+    
+    if (inputString.length === 0) { 
+        opElement.textContent = ""; 
+        return;
+    }
+
     const capitalizedString = inputString[0].toUpperCase() + inputString.slice(1).toLowerCase();
 
-    document.getElementById('intro-name').textContent = `${capitalizedString}`;
+    opElement.textContent = capitalizedString;
+    localStorage.setItem(key, capitalizedString);
+}
+
+function reload(key, place){
+    window.addEventListener('load', ()=> {
+        const intro = document.getElementById(place);
+      const  value = localStorage.getItem(key);
+        intro.innerText= value;
+        
+    })
+
+}
+
+
+
+document.getElementById("input-name").addEventListener('input', () => {
+    
+    const key = "name"
+    display(
+        document.getElementById("input-name"), 
+        document.getElementById("intro-name"),
+        key
+        
+    );
 });
 
+reload("name", "intro-name")
 
-
-// inp_name.addEventListener("input", ()=>{
-    
-//     introName = document.getElementById("intro-name")
-//     display(inp_name, introName)
-// })
 
 inp_profile = document.getElementById("input-profile")
 inp_profile.addEventListener("input", ()=>{
-    
-    introProfile = document.getElementById("intro-profile")
-    display(inp_profile, introProfile)
+    const key = "profile"
+    display(
+        document.getElementById("input-profile"), 
+        document.getElementById("intro-profile"),
+        key
+    );
 })
+
+reload("profile", "intro-profile")
 
 inp_linkedin = document.getElementById("input-linkedin")
 inp_linkedin.addEventListener("input", ()=>{
-    introLinkedin = document.getElementById("intro-linkedin")
-    display(inp_linkedin, introLinkedin)
+    const key = "linkedin"
+    display(
+        document.getElementById("input-linkedin"), 
+        document.getElementById("intro-linkedin"),
+        key
+    );
 })
+
+reload("linkedin", "intro-linkedin")
 
 inp_email = document.getElementById("input-email")
 inp_email.addEventListener("input", ()=>{
-    introEmail = document.getElementById("intro-email")
-    display(inp_email, introEmail)
+    const key = "email"
+    display(
+        document.getElementById("input-email"), 
+        document.getElementById("intro-email"),
+        key
+    );
+    
 })
+
+reload("email", "intro-email")
 
 inp_phone = document.getElementById("input-phone")
 inp_phone.addEventListener("input", ()=>{
-    introPhone = document.getElementById("intro-phone")
-    display(inp_phone, introPhone)
+    const key = "phone"
+    display(
+        document.getElementById("input-phone"), 
+        document.getElementById("intro-phone"),
+        key
+    );
+    
 })
+reload("phone", "intro-phone")
 
 inp_address = document.getElementById("input-address")
 inp_address.addEventListener("input", ()=>{
-    introAddress = document.getElementById("intro-address")
-    display(inp_address, introAddress)
+    const key = "address"
+    display(
+        document.getElementById("input-address"), 
+        document.getElementById("intro-address"),
+        key
+    );
 })
+
+reload("address", "intro-address")
 
 inp_collage_name = document.getElementById("input-collage")
 inp_collage_name.addEventListener("input", ()=>{
-    intro_collage_name = document.getElementById("intro-collage")
-    display(inp_collage_name,intro_collage_name)
+    const key = "collage"
+    display(
+        document.getElementById("input-collage"), 
+        document.getElementById("intro-collage"),
+        key
+    );
 })
+
+reload("collage", "intro-collage")
 
  let inp_collage_address = document.getElementById("input-collage_address")
      inp_collage_address.addEventListener("input", ()=>{
- let   intro_collage_address = document.getElementById("intro-collage_address")
-    display(inp_collage_address,intro_collage_address)
+        const key = "collage_address"
+        display(
+            document.getElementById("input-collage_address"), 
+            document.getElementById("intro-collage_address"),
+            key
+        );
 })
+
+reload("collage_address", "intro-collage_address")
 
 let inp_course = document.getElementById("input-course")
 inp_course.addEventListener("input", ()=>{
-let   intro_course = document.getElementById("intro-course")
-display(inp_course,intro_course)
+    const key = "course"
+    display(
+        document.getElementById("input-course"), 
+        document.getElementById("intro-course"),
+        key
+    );
 })
+
+reload("course", "intro-course")
 
 
 let inp_cgpa = document.getElementById("input-cgpa")
 inp_cgpa.addEventListener("input", ()=>{
-  intro_cgpa = document.getElementById("intro-cgpa")
-display(inp_cgpa,intro_cgpa)
+    const key = "cgpa"
+    display(
+        document.getElementById("input-cgpa"), 
+        document.getElementById("intro-cgpa"),
+        key
+    );
 })
 
+reload("cgpa", "intro-cgpa");
 
-document.getElementById('download').addEventListener('click', () => {
-    const content = document.querySelector('.preview'); // Select the element with class "preview"
 
-    // Download as PDF
-    const { jsPDF } = window.jspdf;
-    const pdf = new jsPDF();
-    // pdf.html(content, {
-    //     callback: function (doc) {
-    //         doc.save('preview.pdf'); // Save the PDF
-    //     },
-    //     x: 10, // Left margin
-    //     y: 10, // Top margin
-    //     width: 190 // Content width for A4
-    // });
 
-    // Download as Image
-const first_name = inp_name.value.split(' ');
-
-    html2canvas(content).then((canvas) => {
-        const link = document.createElement('a');
-
-        link.download = `${first_name[0]} resume`; 
-        link.href = canvas.toDataURL(); // Convert canvas to data URL
-        link.click(); // Trigger the download
-    });
-});
